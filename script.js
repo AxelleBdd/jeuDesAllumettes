a9e440449c19066214712fa5116501868fa56d88
AxelleBdd <axbaudinaud@gmail.com>
2025-03-05 15:59:53 +0100

    Fin etape 4

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
 // DECLARATION DES VARIABLES
 let nombreTotalAllumettes = 50;
+let nombreAllumettes;
 let allumettesAEnlever;
 const affichage = document.querySelector("h2");// permet de sélectioner la phrase dans les balises h2
-const entreeJoueur = document.querySelector("#inputJoueur"); // permet de séléctionner 
-const label = document.querySelector("label");
-const click = document.querySelector("button");
+const entreeJoueur1 = document.querySelector("#inputJoueur1"); // permet de séléctionner l'input du joueur 1
+const entreeJoueur2 = document.querySelector("#inputJoueur2"); // permet de séléctionner l'input du joueur 2
+const clickJoueur1 = document.querySelector("#boutonJoueur1"); // bouton valider du joueur 1
+const clickJoueur2 = document.querySelector("#boutonJoueur2"); // bouton valider du joueur 2
 
-// Assignation du max de l'input
-let inputMax = document.querySelector("#inputJoueur");
+// Assignation du max des l'input
+let inputMax = document.querySelector("input");
 inputMax.setAttribute("max",6);
 
 // DECLARATION DES FONCTIONS
 
 // Fonction permettant de lancer le jeu
-function jeuDesAllumettes() {
-    allumettesAEnlever = choisirNombreAllumettesAEnlever();
+function jeuDesAllumettes(numeroJoueur) {
+    nombreAllumettes = choixDuJoueurAcif(numeroJoueur);
+    allumettesAEnlever = choisirNombreAllumettesAEnlever(nombreAllumettes);
     nombreTotalAllumettes = calculNombreDAllumettes(allumettesAEnlever);
     changerAffichage(nombreTotalAllumettes);
     if (nombreTotalAllumettes == 0) {
-        alert("Vous avez gagné");// s'il n'a a plus d'allumettes c'est la victoire
+        alert(`Le joueur ${numeroJoueur} à gagné`);// s'il n'a a plus d'allumettes c'est la victoire
     };
 }
+// Fonction pour choisir le joueur actif
+function choixDuJoueurAcif(joueur){
+    if(joueur == 1){
+        let allumettes = entreeJoueur1.value;
+        return allumettes;
+    } else if (joueur ==2){
+        let allumettes = entreeJoueur2.value;
+        return allumettes;
+    }
+}
+// Fonction qui permet de récupérer l'entrée de l'utilisateur
+function choisirNombreAllumettesAEnlever(allumettes) {
+    if (allumettes > 6){
+        alert("Le maximum étant 6, votre valeur est maintenant considérée comme un 6");
+        allumettes = 6;
+    } else if (allumettes < 1){
+        alert("Le minimum étant de 1, votre valeur est maintenant considérée comme un 1");
+        allumettes = 1;
+    }
+    return allumettes;
+};
 // Fonction permettant de calculer le total d'allumettes restantes
 function calculNombreDAllumettes(allumettes) {
     if (allumettes>nombreTotalAllumettes){ // si l'utilisateur veut prendre plus d'allumettes que disponibles, on lui force le maximum réstant
@@ -32,22 +56,25 @@ function calculNombreDAllumettes(allumettes) {
 }
 // Fonction permettant de mettre à jour le nombre d'allumettes restantes dans le tas
 function changerAffichage(nombreTotalAllumettes) {
-    affichage.innerText = `Il reste ${nombreTotalAllumettes} allumettes en jeu.`;
+    affichage.innerText = `Il reste ${nombreTotalAllumettes} allumettes dans le tas.`;
 }
-// Fonction qui permet de récupérer l'entrée de l'utilisateur
-function choisirNombreAllumettesAEnlever() {
-    let allumettes = entreeJoueur.value;
-    if (allumettes > 6){
-        alert("Le maximum étant 6, votre valeur est maintenant considérée comme un 6");
-        allumettes = 6;
-    } else if (allumettes < 0){
-        alert("Le minimum étant de 0, votre valeur est maintenant considérée comme un 0");
-        allumettes = 0;
-    }
-    return allumettes;
-};
+
 
 // LANCEMENT DE LA FONCTION DU JEU
-click.addEventListener("click", () => {
-    jeuDesAllumettes();
+// Lorsque le joueur 1 joue
+clickJoueur1.addEventListener("click", () => {
+    jeuDesAllumettes(1);
+    clickJoueur1.disabled = true;
+    entreeJoueur1.disabled = true;
+    clickJoueur2.disabled = false;
+    entreeJoueur2.disabled = false;
+})
+
+// Lorsque c'est le joueur 2 qui joue
+clickJoueur2.addEventListener("click", () => {
+    jeuDesAllumettes(2);
+    clickJoueur1.disabled = false;
+    entreeJoueur1.disabled = false;
+    clickJoueur2.disabled = true;
+    entreeJoueur2.disabled = true;
 })
