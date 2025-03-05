80d31eb295564a06c1274347f2d6517290821698
AxelleBdd <axbaudinaud@gmail.com>
2025-03-05 11:53:24 +0100

    Fin de l'étape 3, ajout d'un input sur l'HTML

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
+// DECLARATION DES VARIABLES
 let nombreTotalAllumettes = 50;
 let allumettesAEnlever;
 const affichage = document.querySelector("h2");// permet de sélectioner la phrase dans les balises h2
+const entreeJoueur = document.querySelector("#inputJoueur"); // permet de séléctionner 
+const label = document.querySelector("label");
+const click = document.querySelector("button");
+
+// Assignation du max de l'input
+let inputMax = document.querySelector("#inputJoueur");
+inputMax.setAttribute("max",6);
+
+// DECLARATION DES FONCTIONS
 
 // Fonction permettant de lancer le jeu
 function jeuDesAllumettes() {
     allumettesAEnlever = choisirNombreAllumettesAEnlever();
     nombreTotalAllumettes = calculNombreDAllumettes(allumettesAEnlever);
     changerAffichage(nombreTotalAllumettes);
-    console.log(nombreTotalAllumettes); // a cause du prompt la phrase h2 n'est pas mise à jour
     if (nombreTotalAllumettes == 0) {
         alert("Vous avez gagné");// s'il n'a a plus d'allumettes c'est la victoire
-    } else { jeuDesAllumettes() } // il reste au moins 1 allumette, on relance la partie
+    };
 }
-
+// Fonction permettant de calculer le total d'allumettes restantes
 function calculNombreDAllumettes(allumettes) {
-    nombreTotalAllumettes = nombreTotalAllumettes - allumettes;
+    if (allumettes>nombreTotalAllumettes){ // si l'utilisateur veut prendre plus d'allumettes que disponibles, on lui force le maximum réstant
+        alert(`Il n'y a pas assez d'allumettes, nous considerons que vous avez selectionné ${nombreTotalAllumettes} allumettes`);
+        allumettes = nombreTotalAllumettes;
+    }
+    nombreTotalAllumettes -= allumettes;
     return nombreTotalAllumettes;
 }
-
+// Fonction permettant de mettre à jour le nombre d'allumettes restantes dans le tas
 function changerAffichage(nombreTotalAllumettes) {
-    affichage.innerText = `Il reste ${nombreTotalAllumettes} allumettes en jeu.`
+    affichage.innerText = `Il reste ${nombreTotalAllumettes} allumettes en jeu.`;
 }
-
+// Fonction qui permet de récupérer l'entrée de l'utilisateur
 function choisirNombreAllumettesAEnlever() {
-    let allumettes = Number(prompt("Combien d'allumettes souhaitez-vous enlever?"));
-    while (allumettes > 6 || allumettes < 1) {
-        allumettes = Number(prompt("Combien d'allumettes souhaitez-vous enlever?(entre 1 et 6)"))
-    }
-    if (allumettes > nombreTotalAllumettes) {
-        allumettes = Number(prompt(`Combien d'allumettes souhaitez-vous enlever? Il reste ${nombreTotalAllumettes}`))
+    let allumettes = entreeJoueur.value;
+    if (allumettes > 6){
+        alert("Le maximum étant 6, votre valeur est maintenant considérée comme un 6");
+        allumettes = 6;
+    } else if (allumettes < 0){
+        alert("Le minimum étant de 0, votre valeur est maintenant considérée comme un 0");
+        allumettes = 0;
     }
     return allumettes;
 };
 
-
-jeuDesAllumettes();
+// LANCEMENT DE LA FONCTION DU JEU
+click.addEventListener("click", () => {
+    jeuDesAllumettes();
+})
